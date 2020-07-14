b2dad8a52702d1f4e3860a0bd7b8319e0765facb
youngpandas <379899580@qq.com>
2020-07-14 10:01:24 +0800

    commit

--- src/offer/demo57二叉树的下一个结点.java
+++ src/offer/demo57二叉树的下一个结点.java
@@ -14,7 +14,13 @@ public class demo57二叉树的下一个结点 {
     }
 
     public TreeLinkNode GetNext(TreeLinkNode pNode) {
-        return null;
+        if (pNode == null) return null;
+        if (pNode.right == null) return null;
+        pNode = pNode.right;
+        while (pNode.left != null) {
+            pNode = pNode.left;
+        }
+        return pNode;
     }
 
 }
